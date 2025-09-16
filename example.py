201d3a948bafe59e7b711b7cd96bcd2b76757478
Kostya Cholak <kostya.cholak@gmail.com>
2025-09-16 11:26:33 +0700

    Patch pyproject.toml

--- example.py
+++ example.py
@@ -1,21 +1,28 @@
+import random
 import time
 
 import explainable
 
 
 def draw(cm: explainable.ContextManager):
-    ctx = cm.get("my_var", default=0)
+    var = cm.get("my_var", default=0)
+    chart_data = cm.get("my_chart_data", default=[0])
 
     return explainable.Graph([
-        explainable.TextNode(f"My var: {ctx}"),
+        explainable.PixelNode({
+            "size": chart_data[-1],
+            "color": "red",
+        })
     ], edges=[])
 
 
 explainable.init(draw)
 explainable.add_context()
 
+my_chart_data = [0]
 my_var = 1
 
 while True:
+    my_chart_data.append(my_chart_data[-1] + random.randint(-10, 10))
     my_var += 1
     time.sleep(0.5)
--- src/explainable/entities.py
+++ src/explainable/entities.py
@@ -1,5 +1,5 @@
-from dataclasses import dataclass
-from typing import Any
+from dataclasses import dataclass, field
+from typing import Any, NotRequired, Optional, TypedDict
 
 
 @dataclass
@@ -12,43 +12,81 @@ class Node:
     is_draggable: bool = True
 
 
+class TextNodeData(TypedDict):
+    text: str
+    width: NotRequired[int]
+    height: NotRequired[int]
+    background: NotRequired[str]
+    foreground: NotRequired[str]
+
+
 @dataclass
 class TextNode(Node):
+    data: TextNodeData = field(default_factory=lambda: TextNodeData(text="", width=None, height=None))
     widget: str = "text"
+
+
+class NumberNodeData(TypedDict):
+    value: float
 
 
 @dataclass
 class NumberNode(Node):
+    data: NumberNodeData = field(default_factory=lambda: NumberNodeData(value=0.0))
     widget: str = "number"
+
+
+class RowNodeData(TypedDict):
+    children: list[Node]
 
 
 @dataclass
 class RowNode(Node):
+    data: RowNodeData = field(default_factory=lambda: RowNodeData(children=[]))
     widget: str = "row"
+
+
+class ColumnNodeData(TypedDict):
+    children: list[Node]
 
 
 @dataclass
 class ColumnNode(Node):
+    data: ColumnNodeData = field(default_factory=lambda: ColumnNodeData(children=[]))
     widget: str = "column"
+
+
+class PixelNodeData(TypedDict):
+    color: str
+    size: NotRequired[int] = None
+    width: NotRequired[int] = None
+    height: NotRequired[int] = None
 
 
 @dataclass
 class PixelNode(Node):
+    data: PixelNodeData = field(default_factory=lambda: PixelNodeData(color="", size=None, width=None, height=None))
     widget: str = "pixel"
+
+
+class LineChartNodeData(TypedDict):
+    data: list[tuple[float, float]]
 
 
 @dataclass
 class LineChartNode(Node):
+    data: LineChartNodeData = field(default_factory=lambda: LineChartNodeData(data=[]))
     widget: str = "linechart"
+
+
+class ClickableExclusiveNodeData(TypedDict):
+    group: str
+    widget: Node
 
 
 @dataclass
 class ClickableExclusiveNode(Node):
-    """
-    data:
-        - group: str
-        - widget: Node
-    """
+    data: ClickableExclusiveNodeData = field(default_factory=lambda: ClickableExclusiveNodeData(group="", widget=TextNode()))
     widget: str = "clickable_exclusive"
 
 
